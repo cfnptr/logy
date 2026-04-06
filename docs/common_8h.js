d7c9ce461e48dea56e563f43ad5a4231f7ffbe34
Nikita Fediuchin <cfnptr@gmail.com>
2026-04-06 18:27:38 +0300

    Release v1.2.2

--- docs/common_8h.js
+++ docs/common_8h.js
@@ -2,5 +2,11 @@ var common_8h =
 [
     [ "LogLevel", "common_8h.html#ae52a0ad1b756f2d7ebc0346d5252bf81", null ],
     [ "LogyResult", "common_8h.html#a00e916a95dd858baf5b7f926e68cbbdf", null ],
-    [ "LogyResult_T", "common_8h.html#a24f3da2dc1aed57e8bb8ec1d1cd5f5a8", null ]
+    [ "LogyResult_T", "common_8h.html#a24f3da2dc1aed57e8bb8ec1d1cd5f5a8", [
+      [ "SUCCESS_LOGY_RESULT", "common_8h.html#a24f3da2dc1aed57e8bb8ec1d1cd5f5a8ab93bbe43e1bbaca73e09c1b9dac414a2", null ],
+      [ "FAILED_TO_ALLOCATE_LOGY_RESULT", "common_8h.html#a24f3da2dc1aed57e8bb8ec1d1cd5f5a8a3230737e3bdf11f5aeb7d6be893ea6c7", null ],
+      [ "FAILED_TO_OPEN_FILE_LOGY_RESULT", "common_8h.html#a24f3da2dc1aed57e8bb8ec1d1cd5f5a8ae289dffecf8f5ecdde95364d23500ebb", null ],
+      [ "FAILED_TO_GET_DIRECTORY_LOGY_RESULT", "common_8h.html#a24f3da2dc1aed57e8bb8ec1d1cd5f5a8adc649c3aa934541995830f939c25a5a2", null ],
+      [ "LOGY_RESULT_COUNT", "common_8h.html#a24f3da2dc1aed57e8bb8ec1d1cd5f5a8adcece10f1d3fdb5e8b76baaca408731e", null ]
+    ] ]
 ];
